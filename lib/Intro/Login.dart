87700448f48795366bf5a205232fb590979f41db
links5925 <kilol0@naver.com>
2023-12-28 01:55:01 +0900

    자동로그인 추가

--- lib/Intro/Login.dart
+++ lib/Intro/Login.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:flutter/cupertino.dart';
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -12,15 +16,18 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   TextEditingController loginController = TextEditingController();
   TextEditingController passwordContoller = TextEditingController();
+  String kakao_token = '';
+  static final storage = new FlutterSecureStorage();
 
-  checkKakao() async {
-
+  get_kakao_token() async {
     // 카카오톡 설치 여부 확인
     // 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
     if (await isKakaoTalkInstalled()) {
       try {
-        await UserApi.instance.loginWithKakaoTalk();
-        print('카카오톡으로 로그인 성공');
+        OAuthToken token = await UserApi.instance.loginWithKakaoTalk();
+        print('카카오톡으로 로그인 성공 ${token.accessToken}');
+        kakao_token = '${token.accessToken}';
+        Navigator.pushNamed(context, '/Select_Screen');
       } catch (error) {
         print('카카오톡으로 로그인 실패 $error');
 
@@ -31,21 +38,57 @@ class _LoginState extends State<Login> {
         }
         // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인
         try {
-          await UserApi.instance.loginWithKakaoAccount();
-          print('카카오계정으로 로그인 성공');
-          Navigator.pushNamed(context, '/');
+          OAuthToken token = await UserApi.instance.loginWithKakaoAccount();
+          print('카카오계정으로 로그인 성공 ${token.accessToken}');
+          kakao_token = '${token.accessToken}';
+          Navigator.pushNamed(context, '/Select_Screen');
         } catch (error) {
           print('카카오계정으로 로그인 실패 $error');
         }
       }
     } else {
       try {
-        await UserApi.instance.loginWithKakaoAccount();
-        print('카카오계정으로 로그인 성공');
-        Navigator.pushNamed(context, '/');
+        OAuthToken token = await UserApi.instance.loginWithKakaoAccount();
+        kakao_token = '${token.accessToken}';
+        print('카카오계정으로 로그인 성공 ${token.accessToken}');
+        Navigator.pushNamed(context, '/Select_Screen');
       } catch (error) {
         print('카카오계정으로 로그인 실패 $error');
       }
+    }
+  }
+
+  post_kakao_user_token() async {
+    final url =
+        Uri.parse('https://basak.chungran.net/v1/auth/kakao/login/finish/');
+    final response = await http.post(url, body: {'access_token': kakao_token});
+    if (response.statusCode == 200) {
+      Map<String, dynamic> responseData =
+          json.decode(utf8.decode(response.bodyBytes));
+      final kakao_access = responseData['access'];
+      final username = responseData['user']['username'];
+      print(responseData);
+      await storage.write(key: 'kakao_token', value: '${kakao_access}');
+      await storage.write(key: 'username', value: '${username}');
+    } else
+      print(response.statusCode);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    read_userinfo();
+  }
+
+  read_userinfo() async {
+    //read 함수를 통하여 key값에 맞는 정보를 불러오게 됩니다. 이때 불러오는 결과의 타입은 String 타입임을 기억해야 합니다.
+    //(데이터가 없을때는 null을 반환을 합니다.)
+    final userInfo = (await storage.read(key: "kakao_token"));
+    print(userInfo);
+
+    //user의 정보가 있다면 바로 로그아웃 페이지로 넘어가게 합니다.
+    if (userInfo != null) {
+      Navigator.pushNamed(context, '/Select_Screen');
     }
   }
 
@@ -145,7 +188,9 @@ class _LoginState extends State<Login> {
                   clipBehavior: Clip.antiAlias,
                   child: ElevatedButton(
                     onPressed: () {
-                      checkKakao().then();
+                      get_kakao_token().then((value) {
+                        post_kakao_user_token();
+                      });
                     },
                     child: Container(),
                     style: ButtonStyle(
