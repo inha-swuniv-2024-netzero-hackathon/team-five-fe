d5037a0cbd4319e7eae3e3528c60171a9972217a
links5925 <kilol0@naver.com>
2023-12-28 04:18:28 +0900

    프로바이더 추가

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,12 @@ import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 import 'package:proto_just_design/Intro/Intro.dart';
 import 'package:proto_just_design/Intro/Login.dart';
 import 'package:proto_just_design/Select_screen.dart';
-import 'package:proto_just_design/profile.dart';
 import 'package:proto_just_design/review.dart';
+import 'package:provider/provider.dart';
+import 'provider.dart';
 
 void main() {
-  KakaoSdk.init(nativeAppKey: 'f6cffbfdf469406747f4e7d4c766db25');
+  KakaoSdk.init(nativeAppKey: '2877c074948b1c5bf00e656eae65ab32');
 
   runApp(const MyApp());
 }
@@ -18,13 +19,26 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => user_info(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => guide_page_data(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => script_page_data(),
+        )
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const MyHomePage(title: 'Flutter Demo Home Page'),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
@@ -43,12 +57,11 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return MaterialApp(
       //debugShowCheckedModeBanner: false, //우측 상단 배너 끄기
-      initialRoute: '/Select_Screen',
+      initialRoute: '/Login',
       routes: {
         '/Intro1': (context) => Intro1(),
         '/Intro2': (context) => Intro2(),
         '/Login': (context) => Login(),
-        '/Profile': (context) => Profile_(),
         '/Review': (context) => review(),
         '/Select_Screen': (context) => selectScreen()
       },
