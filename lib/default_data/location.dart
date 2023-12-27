098f6b30f332b37df51f07747334c4a9c99c2f8c
links5925 <kilol0@naver.com>
2023-12-28 00:27:04 +0900

    장소 세트 설정

---
+++ lib/default_data/location.dart
@@ -0,0 +1,31 @@
+enum location_num {
+  loc1,
+  loc2,
+  loc3,
+  loc4,
+  loc5,
+  loc6,
+  loc7,
+  loc8,
+  loc9,
+  loc10,
+  loc11,
+  loc12,
+  loc13
+}
+
+Map<location_num, List<dynamic>> location_sets = {
+  location_num.loc1: ['도쿄', '신주쿠', 1],
+  location_num.loc2: ['도쿄', '시부야', 2],
+  location_num.loc3: ['도쿄', '긴자', 3],
+  location_num.loc4: ['도쿄', '우에노', 4],
+  location_num.loc5: ['도쿄', '오다이바', 5],
+  location_num.loc6: ['도쿄', '아사쿠사', 6],
+  location_num.loc7: ['도쿄', '롯폰기', 7],
+  location_num.loc8: ['도쿄', '하코네', 8],
+  location_num.loc9: ['도쿄', '요코하마', 9],
+  location_num.loc10: ['도쿄', '가마쿠라', 10],
+  location_num.loc11: ['간사이', '교토', 11],
+  location_num.loc12: ['간사이', '난바', 12],
+  location_num.loc13: ['간사이', '우메다', 13]
+};
